4a8a2f3f6620f5f86b11995b8a23b90e8d7fc880
Hai Dang <haidang@example.com>
2026-02-26 09:11:36 +0700

    Nop bai 1

--- bai1.c
+++ bai1.c
@@ -11,4 +11,29 @@
 
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int nguyenAm = 0, phuAm = 0;
+    printf("Nhap vao mot chuoi: ");
+    gets(str);
+    strupr(str);
+    if (strlen(str) == 0) {
+        printf("Chuoi rong!\n");
+        return 0;
+    }
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'Y') {
+            nguyenAm++;
+        } else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            phuAm++;
+        }
+    }
+
+    printf("So luong nguyen am: %d\n", nguyenAm);
+    printf("So luong phu am: %d\n", phuAm);
+    return 0;
+}
 
